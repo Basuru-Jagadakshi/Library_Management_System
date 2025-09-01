6717386f76156bd656016dcf24a7a8198438a7a6
Basuru-Jagadakshi <jagadakshibasuru@gmail.com>
2025-09-01 17:52:51 +0530

    Add swagger

--- library-management-api/src/main/java/lk/zerocode/library/api/Controller/LibraryController.java
+++ library-management-api/src/main/java/lk/zerocode/library/api/Controller/LibraryController.java
@@ -22,6 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/library")
 @Tag(name = "LibraryController", description = "Perform operations on Library")
+@CrossOrigin(origins = "*")
 public class LibraryController {
 
     @Autowired
@@ -36,7 +37,7 @@ public class LibraryController {
             summary = "POST operation on authors",
             description = "It is used to save authors in the database"
     )
-    @PostMapping(value = "/save-authors")
+    @PostMapping(value = "/authors")
     public void saveAuthor(@Valid @RequestBody CreateAuthorRequestDTO createAuthorRequestDTO){
         libraryService.saveAuthor(createAuthorRequestDTO);
     }
@@ -45,7 +46,7 @@ public class LibraryController {
             summary = "GET operation by using author id",
             description = "It is used to retrieve authors from the database"
     )
-    @GetMapping(value = "get-authors/{id}")
+    @GetMapping(value = "/authors/{id}")
     public AuthorResponseDTO getAuthorById(@PathVariable Long id) throws AuthorNotFoundException{
         return libraryService.getAuthorById(id);
     }
@@ -54,7 +55,7 @@ public class LibraryController {
             summary = "GET operation on author",
             description = "It is used to retrieve authors from the database"
     )
-    @GetMapping(value = "/get-authors")
+    @GetMapping(value = "/authors")
     public List<AuthorResponseDTO> getAuthors(){
         return libraryService.getAuthors();
     }
@@ -63,7 +64,7 @@ public class LibraryController {
             summary = "DELETE operation on author",
             description = "It is used to delete authors from the database"
     )
-    @DeleteMapping(value = "/delete-authors/{id}")
+    @DeleteMapping(value = "/authors/{id}")
     public void deleteAuthor(@PathVariable Long id) throws AuthorNotFoundException{
         libraryService.deleteAuthor(id);
     }
@@ -72,38 +73,38 @@ public class LibraryController {
             summary = "PUT operation on author",
             description = "It is used to update authors in the database"
     )
-    @PutMapping(value = "/update-authors")
+    @PutMapping(value = "/authors")
     public AuthorResponseDTO updateAuthor(@Valid @RequestBody UpdateAuthorRequestDTO updateAuthorRequestDTO) throws AuthorNotFoundException{
         return libraryService.updateAuthor(updateAuthorRequestDTO);
     }
 
 
     //Category methods
-    @PostMapping(value = "/save-category")
+    @PostMapping(value = "/categories")
     @CachePut(cacheNames = "categories", key = "#result.id")
     public Category saveCategory(@Valid @RequestBody CreateCategoryRequestDTO createCategoryRequestDTO){
         return libraryService.saveCategory(createCategoryRequestDTO);
     }
 
-    @GetMapping(value = "/get-category/{id}")
+    @GetMapping(value = "/categories/{id}")
     @Cacheable(value = "categories", key = "#id")
     public CategoryResponseDTO getCategoryById(@PathVariable Long id) throws CategoryNotFoundException{
         return libraryService.getCategoryById(id);
     }
 
-    @GetMapping(value = "/get-categories")
+    @GetMapping(value = "/categories")
     @Cacheable(value = "categories", key = "'all'")
     public List<CategoryResponseDTO> getAllCategory(){
         return libraryService.getAllCategory();
     }
 
-    @DeleteMapping(value = "delete-categories/{id}")
+    @DeleteMapping(value = "/categories/{id}")
     @CacheEvict(value = "categories", key = "#id")
     public void deleteCategory(@PathVariable Long id) throws CategoryNotFoundException{
         libraryService.deleteCategory(id);
     }
 
-    @PutMapping(value = "/update-category")
+    @PutMapping(value = "/categories")
     @CachePut(value = "categories", key = "#updateCategoryRequestDTO.id")
     public CategoryResponseDTO updateCategory(@Valid @RequestBody UpdateCategoryRequestDTO updateCategoryRequestDTO) throws CategoryNotFoundException{
         return libraryService.updateCategory(updateCategoryRequestDTO);
@@ -113,27 +114,27 @@ public class LibraryController {
 
 
     //Member methods
-    @PostMapping(value = "/save-members")
+    @PostMapping(value = "/members")
     public void saveMember(@Valid @RequestBody CreateMemberRequestDTO createMemberRequestDTO){
         libraryService.saveMember(createMemberRequestDTO);
     }
 
-    @GetMapping(value = "/get-members/{id}")
+    @GetMapping(value = "/members/{id}")
     public MemberResponseDTO getMemberById(@PathVariable Long id) throws MemberNotFoundException{
         return libraryService.getMemberById(id);
     }
 
-    @GetMapping(value = "/get-members")
+    @GetMapping(value = "/members")
     public List<MemberResponseDTO> getMembers(){
         return libraryService.getMembers();
     }
 
-    @DeleteMapping(value = "/delete-members/{id}")
+    @DeleteMapping(value = "/members/{id}")
     public void deleteMember(@PathVariable Long id) throws MemberNotFoundException{
         libraryService.deleteMember(id);
     }
 
-    @PutMapping(value = "/update-members")
+    @PutMapping(value = "/members")
     public MemberResponseDTO updateMember(@Valid @RequestBody UpdateMemberRequestDTO updateMemberRequestDTO) throws MemberNotFoundException{
         return libraryService.updateMember(updateMemberRequestDTO);
     }
@@ -141,27 +142,27 @@ public class LibraryController {
 
 
     //Book methods
-    @PostMapping(value = "/save-books")
+    @PostMapping(value = "/books")
     public void saveBook(@Valid @RequestBody CreateBookRequestDTO createBookRequestDTO) throws AuthorNotFoundException, CategoryNotFoundException{
         libraryService.saveBook(createBookRequestDTO);
     }
 
-    @GetMapping(value = "/get-books/{id}")
+    @GetMapping(value = "/books/{id}")
     public BookResponseDTO getBookById(@PathVariable Long id) throws BookNotFoundException{
         return libraryService.getBookById(id);
     }
 
-    @GetMapping(value = "/get-books")
+    @GetMapping(value = "/books")
     public List<BookResponseDTO> getAllBooks(){
         return libraryService.getAllBooks();
     }
 
-    @DeleteMapping(value = "/delete-book/{id}")
+    @DeleteMapping(value = "/books/{id}")
     public void deleteBookById(@PathVariable Long id) throws BookNotFoundException{
         libraryService.deleteBookById(id);
     }
 
-    @PutMapping(value = "/update-book")
+    @PutMapping(value = "/books")
     public BookResponseDTO updateBookById(@Valid @RequestBody UpdateBookRequestDTO updateBookRequestDTO) throws BookNotFoundException, AuthorNotFoundException, CategoryNotFoundException{
         return libraryService.updateBookById(updateBookRequestDTO);
     }
@@ -170,27 +171,27 @@ public class LibraryController {
 
 
     //BorrowedBooks method
-    @PostMapping(value = "/save-borrowed-books")
+    @PostMapping(value = "/borrowed-books")
     public void saveBorrowedBook(@Valid @RequestBody CreateBorrowedBookRequestDTO createBorrowedBookRequestDTO) throws MemberNotFoundException, BookNotFoundException, NoAvailableCopiesException{
         libraryService.saveBorrowedBook(createBorrowedBookRequestDTO);
     }
 
-    @GetMapping(value = "/get-borrowed-books/{id}")
+    @GetMapping(value = "/borrowed-books/{id}")
     public BorrowedBookResponseDTO getBorrowedBookById(@PathVariable Long id) throws BorrowBookNotFoundException{
         return libraryService.getBorrowedBookById(id);
     }
 
-    @GetMapping(value = "/get-borrowed-books")
+    @GetMapping(value = "/borrowed-books")
     public List<BorrowedBookResponseDTO> getBorrowedBook(){
         return libraryService.getBorrowedBook();
     }
 
-    @DeleteMapping(value = "/delete-borrowed-books/{id}")
+    @DeleteMapping(value = "/borrowed-books/{id}")
     public void deleteBorrowedBookById(@PathVariable Long id) throws BorrowBookNotFoundException{
         libraryService.deleteBorrowedBookById(id);
     }
 
-    @PutMapping(value = "/update-borrowed-books")
+    @PutMapping(value = "/borrowed-books")
     public BorrowedBookResponseDTO updateBorrowedBookById(@Valid @RequestBody UpdateBorrowedBookRequestDTO updateBorrowedBookRequestDTO) throws BorrowBookNotFoundException, MemberNotFoundException, BookNotFoundException{
         return libraryService.updateBorrowedBookById(updateBorrowedBookRequestDTO);
     }
